c264ff76ddf063a8a3d6828c6067327164f32bd7
iosアカデミア <yutamac2512@icloud.com>
2025-04-27 20:36:45 +0900

    コミット

---
+++ UlcerativeColitisApp/UlcerativeColitisApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UlcerativeColitisApp
+//
+//  Created by Engineer MacBook Air on 2025/04/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
