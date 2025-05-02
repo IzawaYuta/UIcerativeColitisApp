9cdeef0255ff1f68703db707c6f5314eec864082
iosアカデミア <yutamac2512@icloud.com>
2025-05-02 14:41:55 +0900

    MainTabView追加

---
+++ UlcerativeColitisApp/UlcerativeColitisApp/View/MainTabView.swift
@@ -0,0 +1,25 @@
+//
+//  MainTabView.swift
+//  UlcerativeColitisApp
+//
+//  Created by Engineer MacBook Air on 2025/05/02.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    
+    @State private var selectTab = 0
+    
+    var body: some View {
+        TabView {
+            Tab("1", systemImage: "plus") {
+                HomeView()
+            }
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
