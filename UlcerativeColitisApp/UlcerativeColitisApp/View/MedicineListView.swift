588b1767cf022e038c3d88c46d8288356104edac
yutaizawa <yutamac2512@icloud.com>
2025-05-24 12:16:19 +0900

    MedicineLitView:リファクタリング

--- UlcerativeColitisApp/UlcerativeColitisApp/View/MedicineListView.swift
+++ UlcerativeColitisApp/UlcerativeColitisApp/View/MedicineListView.swift
@@ -12,24 +12,64 @@ struct MedicineListView: View {
     
     @ObservedResults(MedicineDataModel.self) var medicineDataModel
     
+    @State private var editMode: EditMode = .inactive
+    
     var body: some View {
+//        NavigationStack {
+//            VStack {
+//                ForEach(medicineDataModel, id: \.self) { list in
+//                    ZStack {
+//                        RoundedRectangle(cornerRadius: 10)
+//                            .fill(Color.gray.gradient.secondary)
+//                            .frame(maxWidth: .infinity)
+//                            .frame(height: 70)
+//                        HStack {
+//                            Text(list.medicineName)
+//                            Text("\(list.dosage ?? 0)")
+//                            Text("\(list.stock ?? 0)")
+//                        }
+//                    }
+//                    .padding(.horizontal)
+//                }
+//                Spacer()
+//            }
+//            .padding(.vertical)
+//            .environment(\.editMode, $editMode)
+//            .toolbar {
+//                ToolbarItem(placement: .navigationBarTrailing) {
+//                    Text(editMode.isEditing ? "終了" : "編集")
+//                        .onTapGesture {
+//                            if editMode.isEditing {
+//                                editMode = .inactive
+//                            } else {
+//                                editMode = .active
+//                            }
+//                        }
+//                }
+//            }
+//        }
         NavigationStack {
-            VStack {
+            List {
                 ForEach(medicineDataModel, id: \.self) { list in
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 10)
-                            .fill(Color.gray.gradient.secondary)
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 70)
-                        HStack {
-                            Text(list.medicineName)
-                            Text("\(list.dosage ?? 0)")
-                            Text("\(list.stock ?? 0)")
-                        }
+                    HStack {
+                        Text(list.medicineName)
+                        Spacer()
+                        Text("\(list.dosage ?? 0)")
+                        Spacer()
+                        Text("\(list.stock ?? 0)")
                     }
-                    .padding(.horizontal)
+                    .listRowSeparatorTint(.clear)
                 }
+                .padding(.horizontal)
+                .frame(height: 60)
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.cyan.gradient.secondary)
+                )
             }
+            .listStyle(.inset)
+            .padding(.horizontal)
+            .navigationTitle("お薬")
         }
     }
 }
